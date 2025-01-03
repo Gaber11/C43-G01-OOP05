c6f1d572bcbf3c2e59d3ed76865b8f3644c15f7b
Gaber11 <gaberezzat0@gmail.com>
2025-01-03 20:08:29 +0200

    1 - Relational Operators Overloading

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,11 +1,32 @@
-﻿namespace Demo
+﻿using Demo.overloading;
+using System.Numerics;
+using static System.Net.Mime.MediaTypeNames;
+
+
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region 1 - Relational Operators Overloading
+            // Complex c1 = new Complex(1, 2);
+            //  Complex c2 = new Complex(2, 4);
 
+           // Comple C1 = new Comple() { Real = 1, Imag = 2 };
+           // Comple C2 = new Comple() { Real = 2, Imag = 4 };
+          
+           // if (C1 > C2)
+           // {
+           //     Console.WriteLine("C1 > C2");
+           // }
+           //else
+           // {
+           //     Console.WriteLine("C1 < C2");
+           // }
+
+            #endregion
         }
     }
 }
---
+++ Demo/overloading/Comple.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.overloading
+{
+    public class Comple
+    {
+        public int Real { get; set; }
+        public int Imag { get; set; }
+      
+        public override string ToString()
+        {
+            return $"{Real} + {Imag}i";
+        }
+        public static Comple operator +(Comple Left, Comple Right)
+        {
+            return new Comple()
+            {
+                Real = (Left?.Real ?? 0) + (Right?.Real ?? 0),
+                Imag = (Left?.Imag ?? 0) + (Right?.Imag ?? 0)
+            };
+            //left?.Real
+            //Left !=null? Left.Real:null
+        }
+        public static Comple operator -(Comple Left, Comple Right)
+        {
+            return new Comple()
+            {
+                Real = (Left?.Real ?? 0) - (Right?.Real ?? 0),
+                Imag = (Left?.Imag ?? 0) - (Right?.Imag ?? 0)
+            };
+            //left?.Real
+            //Left !=null? Left.Real:null
+        }
+
+        public static bool operator>(Comple Left ,Comple Right)
+        {
+            if (Left.Real == Right.Real)
+                return Left.Imag > Right.Imag;
+            else
+                return Left.Real > Right.Real;
+        }
+        public static bool operator <(Comple Left , Comple Right)
+        {
+            if (Left.Real == Right.Real)
+                return Left.Imag < Right.Imag;
+            else
+                return Left.Real < Right.Real;
+        }
+        
+    }
+}
