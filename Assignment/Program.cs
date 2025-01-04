a9c4a98c9bd8325854dc18c9008481b8e5aa16e9
Gaber11 <gaberezzat0@gmail.com>
2025-01-04 19:39:00 +0200

    First Project:  -Define Class Maths that has four methods: Add, Subtract, Multiply, and Divide, each of them takes two parameters. Call each method in Main ().  Modify the program so that you do not have to create an instance of class to call the four methods.

---
+++ Assignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using Assignment.FirstProject;
+
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region First Project: -Define Class Maths that has four methods: Add, Subtract, Multiply, and Divide, each of them takes two parameters.Call each method in Main().
+
+            //Maths maths = new Maths();
+            //maths.Add(10, 20);
+            //maths.Subtract(10, 20);
+            //maths.Multiply(10, 20);
+            //maths.Divide(40, 20);
+            //--------------------After Modify-------------------
+            //try
+            //{
+            //    Maths.Add(10, 20);
+            //    Maths.Subtract(78, 20);
+            //    Maths.Multiply(19, 20);
+            //    Maths.Divide(0, 0);
+            //}
+            //catch (Exception ex) when (ex is InvalidOperationException
+            //                        || ex is DivideByZeroException)
+
+            //{
+            //    Console.WriteLine($"Exception Cought is : { ex.Message}");
+            //}
+
+
+            #endregion
+
+
+
+        }
+    }
+}
